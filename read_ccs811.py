9c2019e73c04e8dff510766d7d4b78de3ecf7611
Grzegorz on pi plant <grzegorz.zajac@me.com>
2020-04-24 13:49:09 +0200

    add air quality to mqtt

--- read_ccs811.py
+++ read_ccs811.py
@@ -1,15 +1,17 @@
-from time import sleep
+#!/usr/bin/python
 from Adafruit_CCS811 import Adafruit_CCS811
 
 ccs =  Adafruit_CCS811()
 
 while not ccs.available():
-pass
+	pass
 temp = ccs.calculateTemperature()
 ccs.tempOffset = temp - 25.0
+
 if ccs.available():
-temp = ccs.calculateTemperature()
-if not ccs.readData():
-print ('{"CO2":',ccs.geteCO2(),",",'"ppmTVOC":',ccs.getTVOC(),",",'"temp":',temp,"}"
-else:
-print ("ERROR!")
+	    temp = ccs.calculateTemperature()
+	    if not ccs.readData():
+	       print ('{"CO2":', ccs.geteCO2(), ",", '"ppmTVOC":', ccs.getTVOC(), ",", '"temp":',temp,"}")
+
+	    else:
+	      print ("ERROR!")
