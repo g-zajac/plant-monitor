a814c6ad46a22a35d61ef30a748b72193bc89731
Grzegorz on Mac <grzegorz.zajac@me.com>
2020-04-24 11:38:11 +0200

    fix formating

--- read_ccs811.py
+++ read_ccs811.py
@@ -1,17 +1,15 @@
-#this example reads and prints CO2 equiv. measurement, TVOC measurement, and temp every 2 seconds
-
 from time import sleep
 from Adafruit_CCS811 import Adafruit_CCS811
 
 ccs =  Adafruit_CCS811()
 
 while not ccs.available():
-	pass
+pass
 temp = ccs.calculateTemperature()
 ccs.tempOffset = temp - 25.0
 if ccs.available():
-	temp = ccs.calculateTemperature()
-	if not ccs.readData():
-		print ('{"CO2":',ccs.geteCO2(),",",'"ppmTVOC":',ccs.getTVOC(),",",'"temp":',temp,"}"
+temp = ccs.calculateTemperature()
+if not ccs.readData():
+print ('{"CO2":',ccs.geteCO2(),",",'"ppmTVOC":',ccs.getTVOC(),",",'"temp":',temp,"}"
 else:
-	print ("ERROR!")
+print ("ERROR!")
